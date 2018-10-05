49a6a7eff17bd237e50ba2ba5432e086b2f4fba9
sakamotoryuuji <ryuji@st.cs.kumamoto-u.ac.jp>
2018-10-05 15:52:37 +0900

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+import net.librec.common.LibrecException;
+import net.librec.conf.Configuration;
+import net.librec.job.RecommenderJob;
+import parameter.gridSearch;
+import recommend.EfmRecommender;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        testRecommender();
+        //gridtest();
+
+    }
+
+    public static void testRecommender() throws ClassNotFoundException, LibrecException, IOException {
+        Configuration conf = new Configuration();
+        Configuration.Resource resource = new Configuration.Resource("efm-test.properties");
+        Configuration.Resource paraResource = new Configuration.Resource("big_dvd/efm-dvd-parameter.properties");
+        conf.addResource(resource);
+        conf.addResource(paraResource);
+        gridSearch grid = new gridSearch();
+        EfmRecommender recommender = new EfmRecommender();
+        ModifyRecommenderJob job = new ModifyRecommenderJob(conf);
+        job.setParameterSearch(grid);
+        job.setRecommender(recommender);
+        job.runJob();
+    }
+    public static void gridtest() throws ClassNotFoundException, LibrecException, IOException{
+        Configuration conf = new Configuration();
+        Configuration.Resource resource = new Configuration.Resource("grid-test.properties");
+        conf.addResource(resource);
+        parameter.gridSearch grid = new gridSearch();
+        grid.setConf(conf);
+        grid.setup();
+        while (grid.schedule()) {
+            System.out.println(Arrays.toString(grid.getParameterValues()));
+        }
+    }
+}
